4823dcc1e14c4b83578850a090b58a2c323c875d
ckanchanda <107887170+ckanchanda@users.noreply.github.com>
2022-11-24 02:05:35 -0800

    adding file updates

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # API Key
 api_key = "9242feb85e11428d81f20803220111" 
 
-api_key2 = "155c8ec0-60a9-11ed-b59d-0242ac130002-155c8f2e-60a9-11ed-b59d-0242ac130002"
+g_key="AIzaSyCWKy2Sh2W7txiPHxP59KQZXzn9zdWvLiE"
